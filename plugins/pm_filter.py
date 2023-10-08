3485497f6c9446e1c870752bf5fb0eff212b92bc
Thirank <116568300+Thirank@users.noreply.github.com>
2023-10-08 18:08:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -127,27 +127,26 @@ async def next_page(bot, query):
 
         btn.insert(0, 
             [
-                InlineKeyboardButton(f'Sᴇʟᴇᴄᴛ ➢', 'select'),
-                InlineKeyboardButton("ʟᴀɴɢᴜᴀɢᴇs", callback_data=f"languages#{key}"),
-                InlineKeyboardButton("Sᴇᴀsᴏɴs",  callback_data=f"seasons#{key}")
+                InlineKeyboardButton("🪪ʟᴀɴɢᴜᴀɢᴇs🪪", callback_data=f"languages#{key}"),
+                InlineKeyboardButton("📍Sᴇᴀsᴏɴs📍",  callback_data=f"seasons#{key}")
             ]
         )
         btn.insert(0, [
-            InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-            InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+            
+            InlineKeyboardButton("♻️𝐒𝐞𝐧𝐝 𝐀𝐥𝐥♻️", callback_data=f"sendfiles#{key}")
         ])
     else:
         btn = []
         btn.insert(0, 
             [
-                InlineKeyboardButton(f'Sᴇʟᴇᴄᴛ ➢', 'select'),
-                InlineKeyboardButton("ʟᴀɴɢᴜᴀɢᴇs", callback_data=f"languages#{key}"),
-                InlineKeyboardButton("Sᴇᴀsᴏɴs",  callback_data=f"seasons#{key}")
+                InlineKeyboardButton("🪪ʟᴀɴɢᴜᴀɢᴇs🪪", callback_data=f"languages#{key}"),
+                InlineKeyboardButton("📍Sᴇᴀsᴏɴs📍",  callback_data=f"seasons#{key}")
             ]
         )
         btn.insert(0, [
-            InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-            InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+            
+           
+            InlineKeyboardButton("♻️𝐒𝐞𝐧𝐝 𝐀𝐥𝐥♻️", callback_data=f"sendfiles#{key}")
         ])
     try:
         if settings['max_btn']:
@@ -2105,27 +2104,27 @@ async def auto_filter(client, msg, spoll=False):
         ]
         btn.insert(0, 
             [
-                InlineKeyboardButton(f'Sᴇʟᴇᴄᴛ ➢', 'select'),
-                InlineKeyboardButton("ʟᴀɴɢᴜᴀɢᴇs", callback_data=f"languages#{key}"),
-                InlineKeyboardButton("Sᴇᴀsᴏɴs", callback_data=f"seasons#{key}")
+                
+                InlineKeyboardButton("🪪ʟᴀɴɢᴜᴀɢᴇs🪪", callback_data=f"languages#{key}"),
+                InlineKeyboardButton("📍Sᴇᴀsᴏɴs📍", callback_data=f"seasons#{key}")
             ]
         )
         btn.insert(0, [
-            InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-            InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+            
+            InlineKeyboardButton("♻️𝐒𝐞𝐧𝐝 𝐀𝐥𝐥♻️", callback_data=f"sendfiles#{key}")
         ])
     else:
         btn = []
         btn.insert(0, 
             [
-                InlineKeyboardButton(f'Sᴇʟᴇᴄᴛ ➢', 'select'),
-                InlineKeyboardButton("ʟᴀɴɢᴜᴀɢᴇs", callback_data=f"languages#{key}"),
-                InlineKeyboardButton("Sᴇᴀsᴏɴs", callback_data=f"seasons#{key}")
+                
+                InlineKeyboardButton("🪪ʟᴀɴɢᴜᴀɢᴇs🪪", callback_data=f"languages#{key}"),
+                InlineKeyboardButton("📍Sᴇᴀsᴏɴs📍", callback_data=f"seasons#{key}")
             ]
         )
         btn.insert(0, [
-            InlineKeyboardButton("Sᴛᴀʀᴛ Bᴏᴛ", url=f"https://telegram.me/{temp.U_NAME}"),
-            InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}")
+          
+            InlineKeyboardButton("♻️𝐒𝐞𝐧𝐝 𝐀𝐥𝐥♻️", callback_data=f"sendfiles#{key}")
         ])
     if offset != "":
         req = message.from_user.id if message.from_user else 0
